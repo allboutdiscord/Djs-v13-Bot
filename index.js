87795733eb20993d5296b221b9df28d8d6f628ed
allboutdiscord <86099849+allboutdiscord@users.noreply.github.com>
2021-08-11 11:31:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const { Intents } = require("discord.js");
 const client = new Discord.Client({
+  fetchAllMembers: false,
+  allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
   intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-  ],
+    "GUILDS","GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
 });
       
 client.slashcommands = new Discord.Collection();
