d8cd5cff7079b7086240c50353feaa6fe55029e5
allboutdiscord <86099849+allboutdiscord@users.noreply.github.com>
2021-08-11 11:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const Discord = require("discord.js");
-const { Intents } = require("discord.js");
-const client = 
-      
-const normalcommands = new Discord.Collection();
-const slashcommands = new Discord.Collection();
 const config = require("./config.json");
+const { Intents } = require("discord.js");
+const client = new Discord.Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
+});
+      
+client.slashcommands = new Discord.Collection();
+client.normalcommands = new Discord.Collection();
+client.normalaliases = new Discord.Collection();
+["command", "events", "slash"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+}); 
 
 client.login(config.token);
