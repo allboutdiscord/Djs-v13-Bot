880a99dd01f7335103d758a9f4813f18c5396d0e
allboutdiscord <86099849+allboutdiscord@users.noreply.github.com>
2021-08-11 11:20:16 +0530

    Update slash.js

--- handlers/slash.js
+++ handlers/slash.js
@@ -1,6 +1,6 @@
 const { readdirSync } = require("fs");
 const ascii = require("ascii-table");
-let table = new ascii("HARMONY SLASH STATUS");
+let table = new ascii("SLASH STATUS");
 table.setHeading("Command Name", "Command Status");
 module.exports = (client) => {
         const commands = readdirSync(`./slash/`).filter(file => file.endsWith(".js"));
